6d58ef543206b29ea0ae116bb9ca477c636aa280
Suria R Asai <1589044+suriarasai@users.noreply.github.com>
2025-02-17 05:06:59 +0800

    Remove old code
    Removed stale code and loaded latest version pyspark.

--- 01PyCharmPySpark/PySparkTest.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-someFile = "dummy"
-# the above file is under your pythonProject folder
-spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
-print(spark.read.text(someFile).count())
--- PyCharmProject/PySparkDemo.py
+++
@@ -1,4 +0,0 @@
-from pyspark.sql import SparkSession
-filename="dummy.txt"
-spark = SparkSession.builder.appName('Basics').getOrCreate()
-print(spark.read.text(filename).count())
